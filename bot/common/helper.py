0fb970b245c54c3ccb05739d9b463a1865883cec
minhut <uongminh96@gmail.com>
2024-02-18 23:37:17 +0100

    Udate path file

--- bot/common/helper.py
+++ bot/common/helper.py
@@ -1,8 +1,11 @@
 import configparser
+import os
 
+# get the current working directory
+current_working_directory = os.getcwd()
 # Method to read config file settings
 def read_config(path):
     config = configparser.ConfigParser()
-    print("read config from file: " + path)
-    config.read(path)
+    print("read config from file: " + current_working_directory+"/"+path)
+    config.read(current_working_directory+"/"+path)
     return config
