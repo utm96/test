07f18f7d8d0d8a17f4ba29745086aab0c5198052
minhut <uongminh96@gmail.com>
2024-02-18 23:20:32 +0100

    Update tele

--- bot/connector/telegram_connector.py
+++ bot/connector/telegram_connector.py
@@ -6,12 +6,20 @@ config = read_config('configurations.ini')
 BOT_KEY = config['telegram']['BOT_KEY']
 CHAT_ID = config['telegram']['CHAT_ID']
 
+bot_key = '6841144574:AAF2Qxf_90IHC-ZOJxWz0NHSMgq6zgMHu14'
+chat_id = "-4117127789"  # Replace with your chat ID or group ID
+
+
 def send_message_tele(message):
     print("BOT_KEY: " + BOT_KEY)
+    print("BOT_KEY: " + bot_key)
+
     print("CHAT_ID: " + CHAT_ID)
-    bot = telebot.TeleBot(BOT_KEY)
+    print("CHAT_ID: " + chat_id)
+
+    bot = telebot.TeleBot(str(BOT_KEY))
     print(bot)
-    bot.send_message(CHAT_ID, message)
+    bot.send_message(str(CHAT_ID), message)
 
 
 # tele("hello")
