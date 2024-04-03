83d301c69896417840b1b2ed24049f7c61fc96b7
minhut <uongminh96@gmail.com>
2024-04-03 23:49:46 +0200

    update rating

--- bot/analysis/bot.py
+++ bot/analysis/bot.py
@@ -18,17 +18,48 @@ def check_money_flow(symbol):
             money_flow.append(record)
     return money_flow
 
-def check_volume_day(symbol):
+def check_volume_day_and_can_buy(symbol):
     today = date.today()
     sixty_days_ago = today - timedelta(days=60)
     symbol_data = ohlc_data(symbol = symbol,start_date=str(sixty_days_ago),end_date = str(today + timedelta(days=1)),resolution ='D')
     if symbol_data is None:
         return False
     ma20_volume = calculate_ma(symbol_data['volume'], 20)
+    ema9 = calculate_ema(symbol_data['close'], 9)
+    ema5 = calculate_ema(symbol_data['close'], 5)
+    high_current_price = symbol_data['high'].iloc[-1]
 #     if(ma20_volume[-1] <= symbol_data['volume'].iloc[-1] ):
-    return symbol_data['volume'].iloc[-1]/ma20_volume[-1]
+    return symbol_data['volume'].iloc[-1] < ma20_volume[-1] and high_current_price > ema5[-1] and high_current_price > ema9[-1]
 
+def squeez_on(symbol):
+    today = date.today()
+    sixty_days_ago = today - timedelta(days=60)
+    symbol_data = ohlc_data(symbol = symbol,start_date=str(sixty_days_ago),end_date = str(today + timedelta(days=1)),resolution ='D')
+    if symbol_data is None:
+        return False
+    symbol_data['20sma'] = symbol_data['close'].rolling(window=20).mean()
+    symbol_data['stddev'] = symbol_data['close'].rolling(window=20).std()
+    symbol_data['lower_band'] = symbol_data['20sma'] - (2 * symbol_data['stddev'])
+    symbol_data['upper_band'] = symbol_data['20sma'] + (2 * symbol_data['stddev'])
 
+    symbol_data['TR'] = abs(symbol_data['high'] - symbol_data['low'])
+    symbol_data['ATR'] = symbol_data['TR'].rolling(window=20).mean()
+
+    symbol_data['lower_keltner'] = symbol_data['20sma'] - (symbol_data['ATR'] * 1.5)
+    symbol_data['upper_keltner'] = symbol_data['20sma'] + (symbol_data['ATR'] * 1.5)
+
+    def in_squeeze(df):
+        return df['lower_band'] > df['lower_keltner'] and df['upper_band'] < df['upper_keltner']
+
+    symbol_data['squeeze_on'] = symbol_data.apply(in_squeeze, axis=1)
+    count = 0
+    for i in range(1,10):
+        if symbol_data.iloc[-i]['squeeze_on']:
+            count += 1 
+
+    return count 
+
+        
 def check_week(symbol):
     today = date.today()
     start_days = today - timedelta(days=300)
@@ -73,3 +104,18 @@ def check_strong_stock(symbol):
 
 #     if(ma20_volume[-1] <= symbol_data['volume'].iloc[-1] ):
     return ma200[-1] < ma100[-1] < ma50[-1] < ma20[-1] and current_price > ma50[-1]
+
+# def can_buy(symbol):
+#     today = date.today()
+#     four_years_ago = today - timedelta(days=465)
+#     symbol_data = ohlc_data(symbol = symbol,start_date=str(four_years_ago),end_date = str(today+ timedelta(days=1)),resolution ='D')
+#     if symbol_data is None:
+#         return False
+#     current_price = symbol_data['close'].iloc[-1]
+#     ma200 = calculate_ma(symbol_data['close'], 200)
+#     ma100 = calculate_ma(symbol_data['close'], 100)
+#     ma50 = calculate_ma(symbol_data['close'], 50)
+#     ma20 = calculate_ma(symbol_data['close'], 20)
+
+# #     if(ma20_volume[-1] <= symbol_data['volume'].iloc[-1] ):
+#     return ma200[-1] < ma100[-1] < ma50[-1] < ma20[-1] and current_price > ma50[-1]
