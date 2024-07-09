7abd35460b4827261862c151b72c931985f1fb59
minhut <uongminh96@gmail.com>
2024-07-09 11:14:20 +0200

    df

--- bot/test.py
+++ bot/test.py
@@ -9,5 +9,11 @@ from collections import defaultdict
 # calcul_command_price(init_price=31.25,init_vol=25,number_loop=14, step = 0.05, step_vol = 1pp)
 
 
-rsi = check_poc('MSB')
-print(rsi)
+# rsi = check_poc('MSB')
+# print(rsi)
+
+values = [1,2,3]
+def test_func(nums):
+    nums.append(4)
+test_func(values)
+print(values)
